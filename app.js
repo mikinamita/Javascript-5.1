9e3dd23f32b68c7030bcd84b50d44e7aa4a5b539
mikinamita <mikinamita@gmail.com>
2016-12-04 10:12:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-
+//Assignment 5.1
 var name="Namita";
 var age="30";
 var dob="28/06/1985";
 var pofb="Sundargarh";
-
+console.log(name);
+console.log(age);
+console.log(dob);
+console.log(pofb);
+	
